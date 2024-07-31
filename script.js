1f913d9582f6b728ead1d215eb29b5c24919f042
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-07-31 17:25:55 +0200

    discounts

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ console.log('JS OK')
 // 3. Ask the user for the number of kilometers they want to travel
 // 4. Ask the user for his or her age
 // 5. Calculate the total price of the trip
-// 7. Apply discount based on age
-// 8. Insert the ticket price into the DOM
+// 6. Apply discount based on age
+// 7. Insert the ticket price into the DOM
 
 // PREPARATORY PHASE
 // 1. Retrieve the element of my interest from the DOM
@@ -27,7 +27,32 @@ console.log(userAge, typeof userAge);
 
 // PROCESSING PHASE
 // 5. Calculate the total price of the trip
-const mol = kilometerPrice * numKilometers;
-console.log(mol);
+const totalPrice = kilometerPrice * numKilometers;
+console.log(totalPrice);
+
+// 6. Apply discount based on age
+if (userAge <= 18) {
+    console.log('The discount that has been applied to the cost of your ticket is 20%.')
+} else if (userAge >= 65) {
+    console.log('The discount that has been applied to the cost of your ticket is 40%.')
+} else {
+    console.log('No Discount applied')
+}
+
+const firstDiscount = prompt(totalPrice * 20 / 100);
+console.log(firstDiscount);
+
+const secondDiscount = prompt(totalPrice * 40 / 100);
+console.log(secondDiscount);
+
+// Discount 20%
+const youngDiscount = totalPrice - firstDiscount;
+youngDiscount = youngDiscount.toFixed(2);
+console.log(youngDiscount);
+
+// Discount 40%
+const seniorDiscount = totalPrice - secondDiscount;
+seniorDiscount = seniorDiscount.toFixed(2);
+console.log(youngDiscount);
 
 // OUTPUT PHASE
