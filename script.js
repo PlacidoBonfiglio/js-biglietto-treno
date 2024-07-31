37a54bfc027b73765fce170f12dad20dcc024049
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-07-31 18:00:54 +0200

    added console strings to discount

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ console.log(userAge, typeof userAge);
 // PROCESSING PHASE
 // 5. Calculate the total price of the trip
 const totalPrice = kilometerPrice * numKilometers;
-console.log(totalPrice);
+console.log('totalPrice', totalPrice);
 
 // 6. Apply discount based on age
 if (userAge <= 18) {
@@ -41,19 +41,19 @@ if (userAge <= 18) {
 
 // Discount 20%
 const firstDiscount = totalPrice * 20 / 100;
-console.log(firstDiscount);
+console.log('20% discount', firstDiscount);
 
 let youngDiscount = totalPrice - firstDiscount;
 youngDiscount = youngDiscount.toFixed(2);
-console.log(youngDiscount);
+console.log('youngDiscount', youngDiscount);
 
 // Discount 40%
 const secondDiscount = totalPrice * 40 / 100;
-console.log(secondDiscount);
+console.log('40% discount', secondDiscount);
 
 let seniorDiscount = totalPrice - secondDiscount;
 seniorDiscount = seniorDiscount.toFixed(2);
-console.log(seniorDiscount);
+console.log('seniorDiscount', seniorDiscount);
 
 // OUTPUT PHASE
 // 7. Insert the ticket price into the DOM
