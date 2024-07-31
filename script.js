bbd791a0c904dcd489eaf9686d4afdb694bbef30
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-07-31 15:32:41 +0200

    added comments

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('JS OK')
+
+// 1. Retrieve the element of my interest from the DOM
+// 2. Ask the user for the number of kilometers they want to travel
+// 3. Ask the user for his or her age
+// 4. Calculate the total price of the trip
+// 5. Apply discount based on age
+// 6. Insert the ticket price into the DOM
