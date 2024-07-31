6e8dea00da912bb9e9488d971a0e8c04db3e536f
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-07-31 16:13:12 +0200

    preparatory phase

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ console.log('JS OK')
 // 5. Calculate the total price of the trip
 // 7. Apply discount based on age
 // 8. Insert the ticket price into the DOM
+
+// PREPARATORY PHASE
+// 1. Retrieve the element of my interest from the DOM
+const resultElement = document.getElementById('ticket');
+console.log(resultElement);
+
+// 2. Prepare the variable with the cost of the ticket based on the kilometers driven
+const kilometerPrice = 0.21;
+
+// DATA COLLECTION PHASE
+// 3. Ask the user for the number of kilometers they want to travel
+
+
+// PROCESSING PHASE
+
+// OUTPUT PHASE
